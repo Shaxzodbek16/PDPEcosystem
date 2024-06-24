532ce56710c0e08493123b68e6bdf95f9c40dece
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-24 11:16:26 +0500

    6-masalani yecha olmadim

--- Programming-solutions/Leetcode/Python/6-Zigzag-Conversion.py
+++ Programming-solutions/Leetcode/Python/6-Zigzag-Conversion.py
@@ -3,15 +3,13 @@
 
 class Solution:
 	def convert(self, s: str, rows: int) -> str:
-		pass
-
-
-def convert(s: str, rows: int) -> str:
-	for i in range(0, (int(len(s)/3)), rows):
-		print(s[i])
-		print(s[i+1])
-		print(s[i+2])
+		for i in range(0, (int(len(s) / 3)), rows):
+			print(s[i])
+			print(s[i + 1])
+			print(s[i + 2])
+		return "Find"
 
 
 if __name__ == '__main__':
-	convert("PAYPALISHIRING", 3)
+	solution = Solution()
+	print(solution.convert("PAYPALISHIRING", 3))
