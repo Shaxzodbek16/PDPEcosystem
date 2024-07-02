2fc01e500bb7dee6fe19cbcbff03c6a4fdab0d6d
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-07-03 03:32:22 +0500

    yechildi

--- Programming-solutions/Leetcode/Python/8-string-to-integer-(atoi).py
+++ Programming-solutions/Leetcode/Python/8-string-to-integer-(atoi).py
@@ -1,24 +1,34 @@
 # https://leetcode.com/problems/string-to-integer-atoi/
 
 class Solution:
+    def myAtoi(self, s: str) -> int:
+        number = 0
+        is_negative = False
+        sign_received = False
+        number_received = False
+        for char in s:
+            if char == ' ' and not number_received and not sign_received:
+                continue
+            elif char == '-':
+                if sign_received:
+                    break
+                is_negative = True
+                sign_received = True
+            elif char == '+':
+                if sign_received:
+                    break
+                sign_received = True
+            elif char.isdigit():
+                number_received = True
+                sign_received = True
+                number = number * 10 + int(char)
+                if number > 2 ** 31 - 1:
+                    return -2 ** 31 if is_negative else 2 ** 31 - 1
+            else:
+                break
+        return -number if is_negative else number
 
-    def is_first_digit(self, string: str) -> bool:
-        first = string[0]
-        try:
-            first = int(first)
-            return True
-        except ValueError:
-            return False
 
-    def myAtoi(self, string: str) -> int:
-        positive: bool = False
-        if string.startswith("-"): positive = True
-        string = string.strip()
-        if self.is_first_digit(string): return 0
-        making_number: str
-        while self.is_first_digit(string):
-            making_number+=string[0]
-            string=string[1::]
-
-
-
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.myAtoi('-5-'))
