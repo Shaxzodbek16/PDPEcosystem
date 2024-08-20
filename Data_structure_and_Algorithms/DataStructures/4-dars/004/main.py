e0e3c2396569f719f7dc4f79fcc0ebe4a770f730
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-08-20 18:42:02 +0500

    4-dars tugatildi

---
+++ Data_structure_and_Algorithms/DataStructures/4-dars/004/main.py
@@ -0,0 +1,58 @@
+# Task 4. Darsda o`tilgan Stack ning elementlaridan 
+# eng katta qiymatini topadigan funksiya yarating 
+# hamda uning Time Complexity sini aniqlang.
+# public int max()
+
+# from collections import deque # build in stack
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    
+    def __len__(self):
+        return len(self.stack)
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]
+        else:
+            return None
+    
+    def isEmpty(self):
+        return len(self.stack) == 0
+    
+    def push(self, item):
+        self.stack.append(item)
+    
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
+        else:
+            return None
+    
+    # Time Complexity O(1)
+    # Space Complexity O(-)
+    def min(self):
+        if not self.isEmpty():
+            return min(self.stack)
+        else:
+            return None
+    
+    # Time Complexity O(1)
+    # Space Complexity O(-)
+    def max(self):
+        if not self.isEmpty():
+            return max(self.stack)
+        else:
+            return None
+        
+    
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+last = stack.peek()
+print("Last element is:", last)
