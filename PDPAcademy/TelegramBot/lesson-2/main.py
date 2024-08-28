2b37e3cf546b1352a1bada32fab6cfcf04c23b52
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-08-28 17:59:23 +0500

    second-lesson finished

---
+++ PDPAcademy/TelegramBot/lesson-2/main.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+import environ
+environ.Env.read_env('../.env')
+env = environ.Env()
+
+BOT_TOKEN = env('BOT_TOKEN')
+
+TOKEN = BOT_TOKEN
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
