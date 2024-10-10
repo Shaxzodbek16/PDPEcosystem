2f7ec3c132437f91b757e3a5be602e9f3978dbdb
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-10-10 11:03:15 +0500

    1-dars tugatildi

---
+++ PDPAcademy/Flutter/Study/4-modul/1-dars/001/my_home_app/android/app/src/main/kotlin/com/shaxzodbek/my_home_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shaxzodbek.my_home_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ PDPAcademy/Flutter/Study/4-modul/1-dars/001/my_home_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        "Flutter is the framework of dart programming language.",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
