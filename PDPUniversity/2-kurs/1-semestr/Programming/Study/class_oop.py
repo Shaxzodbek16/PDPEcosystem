8fb0f7f4244ae4ec3288af0ad51c78062fbe5444
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-11-07 12:01:58 +0500

    Update class_oop.py

--- PDPUniversity/2-kurs/1-semestr/Programming/Study/class_oop.py
+++ PDPUniversity/2-kurs/1-semestr/Programming/Study/class_oop.py
@@ -3,18 +3,16 @@ class Cats:
     color = 'black'
     age = 2
 
-    def set_name(self, name, color, age):
-        self.name = name
-        self.color = color
-        self.age = age
-
+    def set_name(self, new_name, new_color, new_age):
+        self.name = new_name
+        self.color = new_color
+        self.age = new_age
 
     def meow(self):
         print('Meow', self.name)
 
     def get_name(self):
         return self.name, self.color, self.age
-
 
 
 ct = Cats()
@@ -24,7 +22,7 @@ setattr(ct, 'age', 40)
 # setattr(ct, 'weight', 40)
 print(ct.__dict__)
 print(ct.get_name())
-print(getattr(ct, 'color')) # go to command + tap the func and read docs
+print(getattr(ct, 'color'))  # go to command + tap the func and read docs
 print(getattr(ct, 'name'))
 print(getattr(ct, 'age'))
 print(getattr(ct, 'weight', False))
@@ -39,11 +37,11 @@ class Dog:
     weight: float = None
     breed: str = None
 
-    def set_attributes(self, name, age, weight, breed):
-        self.name = name
-        self.age = age
-        self.weight = weight
-        self.breed = breed
+    def set_attributes(self, other_name, other_age, other_weight, other_breed):
+        self.name = other_name
+        self.age = other_age
+        self.weight = other_weight
+        self.breed = other_breed
 
     def get_attributes(self):
         return self.name, self.age, self.weight, self.breed
@@ -52,6 +50,7 @@ class Dog:
         if self.weight:
             return 'Woof' + self.breed
         return 'Set weight'
+
 
 dog = Dog()
 
@@ -71,11 +70,11 @@ class Singleton:
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
+
 obj1 = Singleton()
 obj2 = Singleton()
 
 print(obj1 is obj2)
-
 
 
 class Point:
@@ -87,10 +86,10 @@ class Point:
     def aggregate_function(self, x, y):
         return self.x + x, self.y + y
 
-
     def __del__(self):
         del self.x, self.y
         print("End")
+
 
 pt = Point(1, 2)
 print(pt.x, pt.y)
@@ -109,12 +108,14 @@ class Rectangle:
     def perimeter(self):
         return 2 * self.width + 2 * self.height
 
+
 rectangle = Rectangle(5, 5)
 print(rectangle.area())
 print(rectangle.perimeter())
 
 from abc import ABC, abstractmethod
 import math
+
 
 class Shape(ABC):
     @abstractmethod
@@ -124,6 +125,7 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass
+
 
 class Circle(Shape):
     def __init__(self, radius):
@@ -137,6 +139,7 @@ class Circle(Shape):
 
     def __str__(self):
         return f"Circle(radius={self.radius})"
+
 
 class Rectangle(Shape):
     def __init__(self, width, height):
@@ -152,12 +155,14 @@ class Rectangle(Shape):
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
 
+
 class Square(Rectangle):
     def __init__(self, side_length):
         super().__init__(side_length, side_length)
 
     def __str__(self):
         return f"Square(side_length={self.width})"
+
 
 class Polygon(Shape):
     def __init__(self, side_length, num_sides):
@@ -175,6 +180,7 @@ class Polygon(Shape):
     def __str__(self):
         return f"Polygon(side_length={self.side_length}, num_sides={self.num_sides})"
 
+
 shapes = [
     Circle(5),
     Rectangle(4, 6),
@@ -186,3 +192,66 @@ for shape in shapes:
     print(shape)
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}\n")
+
+
+class Point:
+    instance_count = 0
+
+    def __new__(cls, instance_count=None, *args, **kwargs):
+        cls.instance_count += 1
+        instance_count += 1
+        return super().__new__(cls)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+pt1 = Point(1, 2)
+pt2 = Point(3, 4)
+pt3 = Point(5, 6)
+print(pt3.instance_count)
+
+
+class Circle:
+    __pi = 3.141592653589793
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def length_circle(self):
+        return 2 * self.radius * self.__pi
+
+
+circle = Circle(5)
+print(circle.length_circle())
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
+
+    def is_square(self):
+        return self.__width == self.__height
+
+    def resize(self, new_width, new_height):
+        self.__width = new_width
+        self.__height = new_height
+
+
+rectangle = Rectangle(5, 5)
+print(rectangle.area())
+print(rectangle.perimeter())
+print(rectangle.is_square())
+rectangle.resize(6, 5)
+
+print(rectangle.area())
+print(rectangle.perimeter())
+print(rectangle.is_square())
