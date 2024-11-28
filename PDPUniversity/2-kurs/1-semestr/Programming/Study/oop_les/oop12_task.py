58401d4eaf453c361defa2d6112c40fbca09b559
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-11-28 14:30:34 +0500

    oop lessons

---
+++ PDPUniversity/2-kurs/1-semestr/Programming/Study/oop_les/oop12_task.py
@@ -0,0 +1,27 @@
+class Bank:
+    def __init__(self, balance: float) -> None:
+        self.__balance = balance
+
+    def add(self, amount: float) -> str:
+        if amount > 0:
+            self.__balance += amount
+            return 'Successfully added to balance'
+        return 'Insufficient balance'
+
+    def get(self, amount: float) -> str:
+        if amount > 0:
+            return 'Invalid amount, it must be positive and more than 0'
+        if amount <= self.__balance:
+            self.__balance -= amount
+            return 'Successfully retrieved from balance'
+        return 'Insufficient balance'
+
+    def balance(self) -> float:
+        return self.__balance
+
+
+bank = Bank(100)
+print(bank.balance())
+print(bank.add(100))
+print(bank.get(150))
+print(bank.balance())
